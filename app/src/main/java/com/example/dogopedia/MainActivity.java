03e0d3ff3c5758389068c66c3db60e50c3d34e78
Alyza Diaz Rodriguez <diazrodrigueza@wit.edu>
2021-04-01 21:18:08 -0400

    Changed tabs to match content and added a splash screen.

--- app/src/main/java/com/example/dogopedia/MainActivity.java
+++ app/src/main/java/com/example/dogopedia/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Dogopedia);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
